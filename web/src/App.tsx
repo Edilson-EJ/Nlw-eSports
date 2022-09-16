1a68d6e0f6b059583ae26adab248a80b2cc50f4d
Edilson-EJ <edilsonjunior624@gmail.com>
2022-09-16 12:33:25 -0300

    react e react native

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,24 +1,47 @@
+import './styles/main.css';
 
-interface buttonProps{
-  title: string;
-}
-
-function Button(props: buttonProps){
-  return (
-    <button>
-      {props.title}
-    </button>
-  )
-}
+import logoImg from './assets/Logo.svg';
 
 function App() {
   return (
-    <div>
-      <Button title="send 1"/>
-      <Button title="send 2"/>
-      <Button title="send 3"/>
-      <Button title="Hello word"/>
-    </div>
+    <div className=' max-w-[1344px] mx-auto flex flex-col items-center my-20'>
+      <img src={logoImg} alt="" />
+
+      <h1 className='text-6xl text-white font-black m-20'>
+        Seu <span className='text-transparent bg-nlw-gradient bg-clip-text'>duo</span> está aqui.
+      </h1>
+
+      <div className="grid grid-cols-6 gap-6 mt-16">
+
+            <a href="">
+              <img src="/game-01.png" alt=''/>
+            </a>
+
+            <a href="">
+              <img src="/game-02.png" alt=''/>
+            </a>
+
+            <a href="">
+              <img src="/game-03.png" alt=''/>
+            </a>
+
+            <a href="">
+              <img src="/game-04.png" alt=''/>
+            </a>
+
+            <a href="">
+              <img src="/game-05.png" alt=''/>
+            </a>
+
+            <a href="">
+              <img src="/game-06.png" alt=''/>
+            </a>
+
+
+      </div>
+          
+
+      </div>
   )
 }
 
